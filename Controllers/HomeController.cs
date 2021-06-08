4c92a42ab7cc2c8824bf4ce57ddef274423c728a
Mark Robinson <markrlv@aol.com>
2021-06-08 13:39:43 -0600

    Add Drop Down Demo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using SandboxJScalc.Models;
 using System;
@@ -23,9 +24,9 @@ namespace SandboxJScalc.Controllers
             return View();
         }
 
-        public ActionResult Recalc(decimal Amount1, decimal Amount2)
+        public ActionResult Recalc(int Amount1, int Amount2)
         {
-            decimal TotalAmount = Amount1 + Amount2;
+            int TotalAmount = Amount1 + Amount2;
             return Json(new { totalamount = TotalAmount });
         }
 
@@ -43,7 +44,31 @@ namespace SandboxJScalc.Controllers
         [HttpPost]
         public ViewResult Index(CommonInfo Common)
         {
-            ModelState.AddModelError("VMessage", "Thank You!");
+            if (Convert.ToString(Request.Form["DropDownTest"]) != null)
+            {
+                DateTime CurrentDate = DateTime.Now;
+                int CurrentYear = CurrentDate.Year;
+
+                Common.MonthList = new();
+                Common.YearList = new();
+
+                for (int i = 1; i <= 12; i++)
+                {
+                    string MonthName = new DateTime(CurrentYear, i, 1).ToString("MMMM");
+                    Common.MonthList.Add(new SelectList(MonthName, MonthName));
+                }
+
+                
+                string YearAsText;
+                YearAsText = (CurrentYear - 1).ToString();
+                Common.YearList.Add(new SelectList(YearAsText, YearAsText));
+                YearAsText = CurrentYear.ToString();
+                Common.YearList.Add(new SelectList(YearAsText, YearAsText));
+                YearAsText = (CurrentYear + 1).ToString();
+                Common.YearList.Add(new SelectList(YearAsText, YearAsText));
+
+                return View("DDdemo", Common);
+            }
             return View("Index");
         }
     }
--- Models/CommonInfo.cs
+++ Models/CommonInfo.cs
@@ -1,14 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
 
 namespace SandboxJScalc.Models
 {
     public class CommonInfo
     {
-        [DisplayFormat(DataFormatString = "{0:n2}", ApplyFormatInEditMode = true)] 
-        public decimal Amount1 { get; set; }
-        [DisplayFormat(DataFormatString = "{0:n2}", ApplyFormatInEditMode = true)]
-        public decimal Amount2 { get; set; }
-        [DisplayFormat(DataFormatString = "{0:n2}", ApplyFormatInEditMode = true)]
-        public decimal TotalAmount { get; set; }
+        public int Amount1 { get; set; }
+        public int Amount2 { get; set; }
+        public int TotalAmount { get; set; }
+        public List<SelectList> MonthList { get; set; }
+        public List<SelectList> YearList { get; set; }
+        public string PeriodCoveredMonth { get; set; }
+        public string PeriodCoveredYear { get; set; }
     }
 }
